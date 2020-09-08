24ce19c498c55392dbbeb44d12ec40e689c56bcb
wolfsbane9011 <mail.g.ankur@gmail.com>
2020-09-08 11:43:55 +0530

    Small changes. Added requirements.txt.

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ A simple crawler to extract data and write it in an excel file after a certain i
 * Prerequisites:
     * Python. If not installed on the system already, then please download it from here, https://www.python.org/downloads/release/python-365/
     
-* Once the repository is cloned, open a command prompt / terminal and execute the command 'pip install requests lxml openpyxl'.
+* Once the repository is cloned, open a command prompt / terminal and execute the command 'pip install -r requirements.txt'.
 * Once all the packages are installed, check the config.py file and make the necessary changes, if any.
 * Then execute the following command 'python crawl.py'. The output files will be created in the output folder after each interval passed in the config.
--- crawl.py
+++ crawl.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import requests as Requests
+import requests
 from lxml import etree
 from openpyxl import Workbook
 from openpyxl.styles import Font
@@ -9,8 +9,10 @@ import config
 
 table_data = dict()
 
+
 def make_request(url):
-    return Requests.get(url, headers=config.REQUEST_HEADERS)
+    return requests.get(url, headers=config.REQUEST_HEADERS)
+
 
 def filter_data(url):
     page = make_request(url)
@@ -21,12 +23,16 @@ def filter_data(url):
     for index, row in enumerate(row_data):
         row_values = list()
         for i, col in enumerate(row.xpath("//table[@id='octable']/tr[" + str(index + 1) + "]/td")):
-            if len(row_data[index].xpath("//table[@id='octable']/tr" + "[" + str(index + 1) + "]" + "/td[" + str(i + 1) + "]//text()")) == 0 and len(row_data[index].xpath("//table[@id='octable']/tr" + "[" + str(index + 1) + "]" + "/td[" + str(i + 1) + "][not(text())]")) > 0:
+            if len(row_data[index].xpath("//table[@id='octable']/tr" + "[" + str(index + 1) + "]" + "/td[" +
+                                         str(i + 1) + "]//text()")) == 0 and \
+                    len(row_data[index].xpath("//table[@id='octable']/tr" + "[" + str(index + 1) + "]" +
+                                              "/td[" + str(i + 1) + "][not(text())]")) > 0:
                 row_values.append("")
             else:
                 path = "//table[@id='octable']/tr" + "[" + str(index + 1) + "]" + "/td[" + str(i + 1) + "]//text()"
                 row_values.append("".join(row_data[index].xpath(path)).strip())
         table_data['row_' + str(index)] = row_values
+
 
 def create_worksheet(filename):
     workbook = Workbook()
@@ -44,11 +50,13 @@ def create_worksheet(filename):
     workbook.save(filename="output/" + filename)
     workbook.close()
 
+
 def start_crawling():
     print (time.ctime())
     filename = config.FILE_PREFIX + "_" + datetime.now().strftime('%d_%m_%Y_%H_%M_%S') + ".xlsx"
     filter_data(config.URL)
     create_worksheet(filename)
+
 
 ticker = threading.Event()
 while not ticker.wait(config.REPORT_INTERVAL):
